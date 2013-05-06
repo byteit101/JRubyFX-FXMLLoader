a22b560c26eee4214d73096d0a3ef39774a81a18
Patrick Plenefisch <simonpatp@gmail.com>
2013-05-06 18:09:44 -0400

    Not being evil and using eval

--- lib/jrubyfx-fxmlloader.rb
+++ lib/jrubyfx-fxmlloader.rb
@@ -439,6 +439,82 @@ class FxmlLoader
 			raise LoadException.new(exception);
 		end
 	end
+  
+  # steal handy methods from activesupport
+      # Tries to find a constant with the name specified in the argument string.
+    #
+    # 'Module'.constantize # => Module
+    # 'Test::Unit'.constantize # => Test::Unit
+    #
+    # The name is assumed to be the one of a top-level constant, no matter
+    # whether it starts with "::" or not. No lexical context is taken into
+    # account:
+    #
+    # C = 'outside'
+    # module M
+    # C = 'inside'
+    # C # => 'inside'
+    # 'C'.constantize # => 'outside', same as ::C
+    # end
+    #
+    # NameError is raised when the name is not in CamelCase or the constant is
+    # unknown.
+    def constantize(camel_cased_word)
+      names = camel_cased_word.split('.')
+      names.shift if names.empty? || names.first.empty?
+
+      names.inject(Object) do |constant, name|
+        if constant == Object
+          constant.const_get(name)
+        else
+          candidate = constant.const_get(name)
+          next candidate if constant.const_defined?(name, false)
+          next candidate unless Object.const_defined?(name)
+
+          # Go down the ancestors to check it it's owned
+          # directly before we reach Object or the end of ancestors.
+          constant = constant.ancestors.inject do |const, ancestor|
+            break const if ancestor == Object
+            break ancestor if ancestor.const_defined?(name, false)
+            const
+          end
+
+          # owner is in Object, so raise
+          constant.const_get(name, false)
+        end
+      end
+    end
+
+    # Tries to find a constant with the name specified in the argument string.
+    #
+    # 'Module'.safe_constantize # => Module
+    # 'Test::Unit'.safe_constantize # => Test::Unit
+    #
+    # The name is assumed to be the one of a top-level constant, no matter
+    # whether it starts with "::" or not. No lexical context is taken into
+    # account:
+    #
+    # C = 'outside'
+    # module M
+    # C = 'inside'
+    # C # => 'inside'
+    # 'C'.safe_constantize # => 'outside', same as ::C
+    # end
+    #
+    # +nil+ is returned when the name is not in CamelCase or the constant (or
+    # part of it) is unknown.
+    #
+    # 'blargle'.safe_constantize # => nil
+    # 'UnknownModule'.safe_constantize # => nil
+    # 'UnknownModule::Foo::Bar'.safe_constantize # => nil
+    def safe_constantize(camel_cased_word)
+      constantize(camel_cased_word)
+    rescue NameError => e
+      raise unless e.message =~ /(uninitialized constant|wrong constant name) #{const_regexp(camel_cased_word)}$/ ||
+        e.name.to_s == camel_cased_word.to_s
+    rescue ArgumentError => e
+      raise unless e.message =~ /not missing constant #{const_regexp(camel_cased_word)}\!$/
+    end
 
 	def getType(name)
 		type = nil
@@ -468,8 +544,7 @@ class FxmlLoader
           # check for ruby
           # TODO: this should require an import or something perhaps? need to think more about this?
           begin
-            # FIXME: don't use eval!
-						type = eval(name)
+						type = constantize(name)
 					rescue 
 						# No-op
 					end
