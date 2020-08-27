fc50c1ee9026e3e30cebd8ff4259ccf15a33d278
Chris Liaw <chrisliaw@antrapol.com>
2020-08-27 21:59:21 +0800

    Remove commented code. We have Git after all. :)

--- lib/fxmlloader/value_elts.rb
+++ lib/fxmlloader/value_elts.rb
@@ -4,8 +4,7 @@ require_relative './elts'
 # Support OpenJFX, requires JRuby version that supports OpenJFX
 # OpenJFX no longer includes javafx.geometry.InsetsBuilder
 # Tested on OpenJFX version 11.0.2, 14.01 & 15 ea on 15th May 2020
-#class StupidFixTODOInsets < Java::javafx::geometry::InsetsBuilder
-class StupidFixTODOInsets #< Java::javafx::geometry::InsetsBuilder
+class StupidFixTODOInsets 
   def initialize()
     super
   end
